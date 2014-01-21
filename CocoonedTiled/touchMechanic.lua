7ffc32e25a2d675a4d94562531375241a7a24bb6
DamagedPandaGame <cocoonedgame@gmail.com>
2014-01-20 16:08:24 -0800

    inverted swipe mechanics so it looks more natural

--- CocoonedTiled/touchMechanic.lua
+++ CocoonedTiled/touchMechanic.lua
@@ -63,29 +63,29 @@ function touchScreen(event, mapData, player)
 		if event.xStart > event.x and swipeLength > 50 then 
 			print("Swiped Left")
 			if mapData.pane == "M" then
-				mapData.pane = "L"
-			elseif mapData.pane == "R" then
+				mapData.pane = "R"
+			elseif mapData.pane == "L" then
 				mapData.pane = "M"
 			end
 		elseif event.xStart < event.x and swipeLength > 50 then 
 			print( "Swiped Right" )
 			if mapData.pane == "M" then
-				mapData.pane = "R"
-			elseif mapData.pane == "L" then
+				mapData.pane = "L"
+			elseif mapData.pane == "R" then
 				mapData.pane = "M"
 			end
 		elseif event.yStart > event.y and swipeLengthy > 50 then
 			print( "Swiped Down" )
 			if mapData.pane == "M" then
-				mapData.pane = "D"
-			elseif mapData.pane == "U" then
+				mapData.pane = "U"
+			elseif mapData.pane == "D" then
 				mapData.pane = "M"
 			end
 		elseif event.yStart < event.y and swipeLengthy > 50 then
 			print( "Swiped Up" )
 			if mapData.pane == "M" then
-				mapData.pane = "U"
-			elseif mapData.pane == "D" then
+				mapData.pane = "D"
+			elseif mapData.pane == "U" then
 				mapData.pane = "M"
 			end
 		end	
