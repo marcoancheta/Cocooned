3d5b2cf992eddfcc6f72a4b2b2267d028d1eea57
Derrick Wong <decwong@ucsc.edu>
2014-02-09 15:35:05 -0800

    Added coin sprite sheet and fixed selectLevel merge

--- CocoonedTiled/selectLevel.lua
+++ CocoonedTiled/selectLevel.lua
@@ -179,25 +179,12 @@ local function selectLoop(event)
 		levels[i]:setFillColor(0, 0, 0)
 		map.layer["tiles"]:insert(kCircle[i])
 		map.layer["tiles"]:insert(levels[i])
-<<<<<<< HEAD
 		
 		-- Unlock && lock levels
 		if i~=1 and i~=2 and i~=3 and 
 		   i~=4 and i~=5 and i~=6 and 
 		   i~=8 and i~=16 then
-=======
-	end
-					
-	kCircle[1].isAwake = true
-	selectLevel.levelNum = kCircle[1].name
-	kCircle[1]:setFillColor(167*0.00392156862, 219*0.00392156862, 216*0.00392156862)
-	
-	
-	lockedLevels = {}
-	
-	for i=1, #lvlNumber do
-		if i~= 1 and i~=2 and i~=3 and i~=4 and i~=5 then
->>>>>>> 1abbd96ff95a20c424f7632db1f3132acad15f90
+		   
 			lockedLevels[i] = display.newImage("graphics/lock.png")
 			lockedLevels[i].x = kCircle[i].x
 			lockedLevels[i].y = kCircle[i].y
