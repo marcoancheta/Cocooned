069d06ebd7951ed1d2051c76efb5197741648fe7
DamagedPandaGame <cocoonedgame@gmail.com>
2014-02-21 12:19:58 -0800

    fixed exit portal bug

--- CocoonedTiled/Objects/exitPortalCollision.lua
+++ CocoonedTiled/Objects/exitPortalCollision.lua
@@ -2,10 +2,11 @@ local sound = require("sound")
 
 function collide(collideObject, player, event, mapData, map)
 	event.contact.isEnabled = false
-	audio.stop()
-	sound.playSound(event, sound.portalOpeningSound)
+	
 	if collideObject.sequence == "move" then
-		gameData.gameEnd = true
+		audio.stop()
+		sound.playSound(event, sound.portalOpeningSound)
+		timer.performWithDelay(1000, function() gameData.gameEnd = true end)
 	end
 end
 
