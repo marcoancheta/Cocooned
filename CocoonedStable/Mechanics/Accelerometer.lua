25bf6f8e5de5a156c09854f0353ede3982f89c53
Derrick Wong <decwong@ucsc.edu>
2014-04-20 01:50:04 -0700

    Still adjusting impulse values via device.

--- CocoonedStable/Mechanics/Accelerometer.lua
+++ CocoonedStable/Mechanics/Accelerometer.lua
@@ -140,7 +140,7 @@ local function onAccelerate(event, player)
 			highestxgrav = xGrav
 		end
 		
-		ball:applyLinearImpulse(xGrav * 0.5, yGrav * 0.5 , ball.x, ball.y)
+		ball:applyLinearImpulse(xGrav * 0.85, yGrav * 0.85 , ball.x, ball.y)
 		timer.performWithDelay(200, cancelDeathTimer)
 		
 	elseif event.isShake ~= true then
