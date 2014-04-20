20c69a0cbcf835cf18f6beef7dd203278a3b79be
Derrick Wong <decwong@ucsc.edu>
2014-04-20 06:03:04 -0700

    Almost there.

--- CocoonedStable/Mechanics/Accelerometer.lua
+++ CocoonedStable/Mechanics/Accelerometer.lua
@@ -107,6 +107,12 @@ local function onAccelerate(event, player)
 		end
 		
 		if gameData.inWater == false then
+			txtObj.text = "false"
+			txtObj.x = display.contentCenterX
+			txtObj.y = display.contentCenterY
+			txtObj:setFillColor(0,1,0)
+			txtObj:toFront()
+		
 			-- offset the gravity to return
 			physicsParam.xGrav = xGrav
 			physicsParam.yGrav = yGrav
