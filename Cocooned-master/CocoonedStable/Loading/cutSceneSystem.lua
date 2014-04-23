09ca6f0fc9c14707705340c89d5d3e919fee15d4
Andrew Temena <a.j.temena@gmail.com>
2014-04-23 14:36:40 -0700

    fixed cutscenes

--- Cocooned-master/CocoonedStable/Loading/cutSceneSystem.lua
+++ Cocooned-master/CocoonedStable/Loading/cutSceneSystem.lua
@@ -1,6 +1,6 @@
 --TODO: add in levels that have cutscenes
 --TODO: Add Variable to gameData to show that player is in cutscene?
-local numberOfScreens= {[1]=3} --level number, number of cutscenes
+local numberOfScreens= {["LS"] = 0, ["1"] = 3, ["2"] = 0, ["3"] = 0, ["4"] = 0, ["5"] = 0} --level number, number of cutscenes
 local levNum = 0
 local currScene = 0
 local loadingBG={}
@@ -23,11 +23,13 @@ end
 local function cutScene(levelNumber, gui)
 	localGui = gui
 	levNum = levelNumber
-	for i= numberOfScreens[levelNumber],1,-1 do
-		loadingBG[i] = display.newImage("mapdata/art/cutscenes/OLD/level"..levelNumber .. "Scene" .. i .. ".png", 10, 20,true )
+	if numberOfScreens[levelNumber] > 0 then
+		for i= numberOfScreens[levelNumber],1,-1 do
+			loadingBG[i] = display.newImage("mapdata/art/cutscenes/OLD/level"..levelNumber .. "Scene" .. i .. ".png", 700, 432,true )
+		end
+		localGui.back:addEventListener("tap", nextSceneOrDelete)
+		currScene = 1
 	end
-	localGui.back:addEventListener("tap", nextSceneOrDelete)
-	currScene = 1
 end
 
 
