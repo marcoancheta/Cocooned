f755d262164fb310f707ef74d65e7f0e406b30e0
decwong <decwong@ucsc.edu>
2014-04-25 17:42:56 -0700

    Dusk is gone and forgotten. RIP.

--- Cocooned-master/CocoonedStable/Objects/redAuraCollision.lua
+++ Cocooned-master/CocoonedStable/Objects/redAuraCollision.lua
@@ -18,12 +18,12 @@ local sound = require("sound")
 -- Updated by: Marco
 --------------------------------------------------------------------------------
 local function collide(collideObject, player, event, mapData, map, gui)
-	player = player
+	--player = player
 	event.contact.isEnabled = false
 	player:changeColor('red')
 	
-	local closure = function() return player:changeColor('white') end
-	timer1 = timer.performWithDelay( 10000, closure, 1)
+	--local closure = function() return player:changeColor('white') end
+	--timer1 = timer.performWithDelay( 10000, closure, 1)
 end
 
 --------------------------------------------------------------------------------
