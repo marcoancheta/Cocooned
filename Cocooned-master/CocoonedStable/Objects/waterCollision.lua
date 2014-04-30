660658500a887e7051ed5aa53713244e816418e5
Derrick Wong <decwong@ucsc.edu>
2014-04-30 03:47:35 -0700

    Trying to fix water collision

--- Cocooned-master/CocoonedStable/Objects/waterCollision.lua
+++ Cocooned-master/CocoonedStable/Objects/waterCollision.lua
@@ -16,7 +16,7 @@ local gameData = require("Core.gameData")
 -- Updated by: Andrew moved event.contact.isenabled to precollision
 --------------------------------------------------------------------------------
 local function collide(collideObject, player, event, mapData, map, gui)
-	--event.contact.isEnabled = true
+	event.contact.isEnabled = true
 
 	-- play sound
 	--sound.playSound(sound.soundEffects[4])	
