1b06b5709b63beb32753e1298a24e69ed8898e58
Derrick Wong <decwong@ucsc.edu>
2014-05-13 16:47:35 -0700

    Fixed water splash animation bug.

--- Cocooned-master/CocoonedStable/Objects/waterCollision.lua
+++ Cocooned-master/CocoonedStable/Objects/waterCollision.lua
@@ -3,14 +3,27 @@
 -- Cocooned by Damaged Panda Games (http://signup.cocoonedgame.com/)
 -- waterCollision.lua
 --------------------------------------------------------------------------------
+local gameData = require("Core.gameData")
+local sound = require("sound")
+local animation = require("Core.animation")
 --------------------------------------------------------------------------------
 -- Variables
 --------------------------------------------------------------------------------
 -- Updated by: Marco
 --------------------------------------------------------------------------------
-local gameData = require("Core.gameData")
-local sound = require("sound")
-local animation = require("Core.animation")
+local splashAnim
+
+--------------------------------------------------------------------------------
+-- Clean Function - function for deleting all local variables
+--------------------------------------------------------------------------------
+local function clean(event)
+	local params = event.source.params
+	
+	if params.splashParams then
+		params.splashParams:removeSelf()
+		params.splashParams = nil
+	end
+end
 
 --------------------------------------------------------------------------------
 -- Collide Function - function for water collision
@@ -19,30 +32,30 @@ local animation = require("Core.animation")
 --------------------------------------------------------------------------------
 local function collide(collideObject, player, event, mapData, map, gui)
 	--event.contact.isEnabled = true
-
-	-- play sound
-	--sound.playSound(sound.soundEffects[4])	
 	
+	-- If player is not on top of iceberg
 	if gameData.onIceberg == false then
 		-- play sound
 		sound.stopChannel(1)
 	    sound.playSound(sound.soundEffects[4])
-	    local splashAnim = display.newSprite(animation.sheetOptions.splashSheet, animation.spriteOptions.splash)	
-		  -- Start wolf off-screen
-		  splashAnim.x = collideObject.x
-		  splashAnim.y = collideObject.y + 280
-		  -- Enlarge the size of the splash
-		  -- splashAnim:scale(1, 3)
-		  splashAnim:setSequence("move")
-		  splashAnim:play()
-		  if ( event.phase == "ended" ) then
-		  	splashAnim:removeSelf( )
-		  end
+	    splashAnim = display.newSprite(animation.sheetOptions.splashSheet, animation.spriteOptions.splash)	
+		-- Start wolf off-screen
+		splashAnim.x = player.imageObject.x
+		splashAnim.y = player.imageObject.y
+		-- Enlarge the size of the splash
+		-- splashAnim:scale(1, 3)
+		splashAnim:setSequence("move")
+		splashAnim:play()
+		-- Create timer to remove splashAnimation
+		local timer = timer.performWithDelay(500, clean)
+		timer.params = {splashParams = splashAnim}
 		-- reset player's aura and movement
 		player:changeColor("white")
 		--player.movement ="inWater"
 		gameData.inWater = true
 		player.imageObject.linearDamping = 8
+		--splashAnim:removeSelf()
+		--splashAnim = nil
 	end
 end
 
