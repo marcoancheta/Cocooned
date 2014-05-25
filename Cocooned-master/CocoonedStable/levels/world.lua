47f5df6971f4959d1762038e19550d3d92a45a04
John Bentley <jwbentle@ucsc.edu>
2014-05-24 21:48:16 -0700

    Placed level portals back

--- Cocooned-master/CocoonedStable/levels/world.lua
+++ Cocooned-master/CocoonedStable/levels/world.lua
@@ -87,7 +87,7 @@ local function load(mapData, map, rune, objects, wisp, water, wall, auraWall)
 		objects["exitPortal3"].x, objects["exitPortal3"].y = generate.tilesToPixels(31, 15)
 		
 		-- Corona Simulator Accel Coordinates:
-		objects["exitPortal3"].x, objects["exitPortal3"].y = generate.tilesToPixels(14, 12)	
+		--objects["exitPortal3"].x, objects["exitPortal3"].y = generate.tilesToPixels(14, 12)	
 
 		
 		-- Play animation for all world portals
