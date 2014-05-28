89755988be892c1a01e5a99f311322f223fa231b
Derrick Wong <decwong@ucsc.edu>
2014-05-27 23:57:25 -0700

    Optimizing accelerometer function; backed-up Marco's version "accelerometer2"

--- Cocooned-master/CocoonedStable/Mechanics/Accelerometer.lua
+++ Cocooned-master/CocoonedStable/Mechanics/Accelerometer.lua
@@ -33,7 +33,9 @@ local accelPlayer = {
 	[1] = nil,
 	[2] = nil
 }
-
+--------------------------------------------------------------------------------
+-- emptyGroup(displayGroup) - clear out rayCastCheck group
+--------------------------------------------------------------------------------
 local function emptyGroup(displayGroup)
 	if displayGroup.numChildren > 0 then
 		for i = displayGroup.numChildren, 1, -1 do
@@ -43,36 +45,9 @@ local function emptyGroup(displayGroup)
 end
 
 --------------------------------------------------------------------------------
--- Cancel Death Timer - function that cancels end game from being changed
+-- calcAccelValues - calculate Accelerometer gravity values
 --------------------------------------------------------------------------------
--- Updated by: Andrew
---------------------------------------------------------------------------------
---[[
-local function cancelDeathTimer() 
-	for i=1, #accelPlayer do
-		if accelPlayer[i].movement == "accel" and accelPlayer[i].deathTimer ~= nil then 
-			timer.cancel(accelPlayer[i].deathTimer) 
-			accelPlayer[i].deathTimer = nil  
-			accelPlayer[i].imageObject.linearDamping = 1.25 
-			accelPlayer[i].speedConst = accelPlayer[i].defaultSpeed
-			--accelPlayer.speedUpTimer = timer.performWithDelay(5000, function() accelPlayer.speedConst = 10 end)
-			accelPlayer[i].deathScreen:pause()
-			accelPlayer[i].deathScreen:removeSelf()
-			accelPlayer[i].deathScreen = nil
-		end
-	end
-end
-]]--
---------------------------------------------------------------------------------
--- On Accelerate - function that gathers accelerometer data
---------------------------------------------------------------------------------
--- Updated by: Derrick
--- Previous: Andrew 
---------------------------------------------------------------------------------
-local function onAccelerate(event, player)
-	-- Print escape path
-	--print(player.escape)
-		
+local function calcAccelValues(event)
 	-- Accelerometer Tilt Events	
 	local xGrav = 0
 	local yGrav = 0
@@ -105,190 +80,171 @@ local function onAccelerate(event, player)
 		yGrav = 0
 		--sound.stopChannel(2)
 	end
-		
-	-- Accelerometer Shake Event
-	if event.isShake and gameData.inWater == true and player.shook == false then
-		--print(" IM FUCKING SHAKING NIGGA!!! ")
-		
-		emptyGroup(rayCastCheck)
-		emptyGroup(rayCastDistanceCheck)
-		-- emptyGroup(lastPointCheck)
-		-- emptyGroup(lastPointDistanceCheck)
+	
+	return xGrav, yGrav
+end
 
-
-		local lastPoint = player.lastSavePoint
-		if lastPoint == nil then
-			print("I DONT EVEN HAVE A LAST POINT")
+--------------------------------------------------------------------------------
+-- launch(rayCastCheck, ball, player, useLastPoint) - 
+--------------------------------------------------------------------------------
+local function launch(rayCastCheck, ball, player, useLastPoint)
+	local choosePoint = 0
+	local minDist = math.huge
+	
+	for i = 1, rayCastCheck.numChildren do
+		local dist
+		if useLastPoint then
+			dist = uMath.distance(rayCastCheck[i], lastPoint)
+			--print("Hit: " .. i .. " " .. rayCastCheck[i].name .. " at position " .. rayCastCheck[i].x .. ", " .. rayCastCheck[i].y .. " distance: " .. dist)
 		else
-			print("I died on pane : " .. lastPoint.pane)
+			dist = uMath.distance(rayCastCheck[i], ball)
+			--print("Hit: " .. i .. " " .. rayCastCheck[i].name .. " at position " .. rayCastCheck[i].x .. ", " .. rayCastCheck[i].y .. " distance: " .. dist)
 		end
-
-		local useLastPoint = true
-		if player.lastSavePoint.moveable then
-			useLastPoint = false
+		
+		if dist < minDist then
+			minDist = dist
+			choosePoint = i
 		end
-		--print("Player last position at : " .. player.lastPositionX .. ", " .. player.lastPositionY)
+	end	
 
-		-- local lpDegree = 0
-		-- if useLastPoint then
+	--print("Go to point : " .. choosePoint .. " at " .. rayCastCheck[choosePoint].x .. ", " .. rayCastCheck[choosePoint].y .. "with distance of " .. minDist)
+	rayCastCheck[choosePoint]:setFillColor(0,0,0)
+	rayCastCheck[choosePoint]:toFront()
 
-		-- 	for i = 1, 36 do
-		-- 		local x = lastPoint.x + (50 * math.cos(lpDegree * (math.pi/180)))
-		-- 		local y = lastPoint.y + (50 * math.sin(lpDegree * (math.pi/180)))
-		-- 		-- local checkCircle = display.newCircle(x, y, 5)
-		-- 		-- checkCircle:setFillColor(1,0,1)
-		-- 		-- checkCircle.name = "check"
-		-- 		-- lastPointDistanceCheck:insert(checkCircle)
-		-- 		-- lastPointDistanceCheck:toFront()
+	local pushDistance = uMath.distance(ball, rayCastCheck[choosePoint])
+	local straightDistance = uMath.distance(ball, lastPoint)
+	local chooseDistance = pushDistance
+	local chooseLocation = rayCastCheck[choosePoint]
 
-		-- 		local hits = physics.rayCast(lastPoint.x, lastPoint.y, x, y, "sorted")
-		-- 		if ( hits ) then
-		-- 		    -- Output all the results.
-		-- 		    for i,v in ipairs(hits)
-		-- 		    do
-		-- 		    	if v.object.name == "background" or v.object.name == "iceberg" then
-		-- 			    	local pointC = display.newCircle(v.position.x, v.position.y, 10)
-		-- 			    	pointC:setFillColor(1,1,0)
-		-- 			    	pointC.name = v.object.name
-		-- 			    	lastPointCheck:insert(pointC)
-		-- 			    	lastPointCheck:toFront()
-		-- 			    end
-				        
-		-- 		    end					
-		-- 		else
-		-- 		    -- There's no hit.
-		-- 		end
-				
-		-- 		lpDegree = lpDegree + 10
-		-- 	end
-		-- end
-		-- lastPointCheck:toFront()
+	if pushDistance > straightDistance and useLastPoint then
+		chooseLocation = lastPoint
+		chooseDistance = straightDistance
+	end
 
-		local ball = player.imageObject		
-		local degree = 0
-		local distanceCheck = 50
-		local shoreFound = false
-		local rotation = 36
-		local degreeAdd = 10
-		local foreverCheck = 0
+	local deltaX = chooseLocation.x - ball.x
+	local deltaY = chooseLocation.y - ball.y
+	local angleX = math.acos(deltaX/chooseDistance)
+	local angleY = math.asin(deltaY/chooseDistance)
+	local jumpDirectionX = 20 * math.cos(angleX)
+	local jumpDirectionY = 20 * math.sin(angleY)
 
-		while  shoreFound == false do
-			foreverCheck = foreverCheck + 1
-			for i = 1, rotation do
-				local x = ball.x + (distanceCheck * math.cos(degree * (math.pi/180)))
-				local y = ball.y + (distanceCheck * math.sin(degree * (math.pi/180)))
-				-- local checkCircle = display.newCircle(x, y, 5)
-				-- checkCircle:setFillColor(0,1,0)
-				-- checkCircle.name = "check"
-				-- rayCastDistanceCheck:insert(checkCircle)
-				-- rayCastDistanceCheck:toFront()
+	--print("push player in " .. jumpDirectionX .. ", " .. jumpDirectionY)
+	accelPlayer[1] = player
+	player.shook = true
+	ball.linearDamping = 0
+	ball:setLinearVelocity(deltaX*3, deltaY*3)
+	ball.alpha = 1
+end
 
-				local hits = physics.rayCast(ball.x, ball.y, x, y, "sorted")
-				if ( hits ) then
-				    -- Output all the results.
-				    for i,v in ipairs(hits)
-				    do
-				    	if v.object.name == "background" or v.object.name == "iceberg" then
-					    	local pointC = display.newCircle(v.position.x, v.position.y, 1)
-					    	--pointC:setFillColor(1,0,0)
-					    	pointC.alpha = 0
-					    	pointC.name = v.object.name
-					    	rayCastCheck:insert(pointC)
-					    	rayCastCheck:toFront()
-					    end
-				        
-				    end					
-				else
-				    -- There's no hit.
-				end
-				
-				degree = degree + degreeAdd
-			end
-			if(distanceCheck >= 200) then
-				degreeAdd = 5
-				rotation = 72
-			end
-			degree = 0
+--------------------------------------------------------------------------------
+-- shake(event) - 
+--------------------------------------------------------------------------------
+local function shake(player)
+	-- Object values
+	local lastPoint = player.lastSavePoint
+	local ball = player.imageObject	
+	-- Integers
+	local degree = 0
+	local distanceCheck = 50
+	local rotation = 36
+	local degreeAdd = 10
+	-- Booleans
+	local useLastPoint = true
+	local shoreFound = false
+	--local foreverCheck = 0
+	
+	---------------------------
+	------- DEBUG TEXT --------
+	--if lastPoint == nil then
+	--	print("I DONT EVEN HAVE A LAST POINT")
+	--else
+	--	print("I died on pane : " .. lastPoint.pane)
+	--end
+	------- DEBUG TEXT --------
+	---------------------------
+	
+	if player.lastSavePoint.moveable then
+		useLastPoint = false
+	end
+	
+	-- Keep checking for position until water is found
+	while shoreFound == false do
+		--foreverCheck = foreverCheck + 1		
+		for i = 1, rotation do
+			local x = ball.x + (distanceCheck * math.cos(degree * (math.pi/180)))
+			local y = ball.y + (distanceCheck * math.sin(degree * (math.pi/180)))
 
-			for i = 1, rayCastCheck.numChildren do
-				if useLastPoint then
-					local pointToSafety = uMath.distance(lastPoint, rayCastCheck[i])
-					--print("checking distance: " .. pointToSafety)
-					if math.abs(pointToSafety) < 80 then
-						shoreFound = true
-						break
+			local hits = physics.rayCast(ball.x, ball.y, x, y, "sorted")
+			if ( hits ) then
+			    -- Output all the results.
+			    for i,v in ipairs(hits) do
+				    if v.object.name == "background" or v.object.name == "iceberg" then
+						local pointC = display.newCircle(v.position.x, v.position.y, 1)
+						--pointC:setFillColor(1,0,0)
+						pointC.alpha = 0
+						pointC.name = v.object.name
+						rayCastCheck:insert(pointC)
+						rayCastCheck:toFront()
 					end
+				end					
+			end	
+			degree = degree + degreeAdd
+		end
+					
+		for i = 1, rayCastCheck.numChildren do
+			if useLastPoint then
+				local pointToSafety = uMath.distance(lastPoint, rayCastCheck[i])
+				--print("checking distance: " .. pointToSafety)
+				if math.abs(pointToSafety) < 80 then
+					shoreFound = true
+					break
 				end
 			end
-			distanceCheck = distanceCheck + 25
-			if foreverCheck > 20 then
-				--print("you just hit a forever loop, we getting out of here!!")
-				break
-			end
-			if shoreFound ~= true then
-				emptyGroup(rayCastDistanceCheck)
-				emptyGroup(rayCastCheck)
-			end
 		end
-		rayCastCheck:toFront()
-		--print("display group has " .. rayCastCheck.numChildren .. " objects")
+					
+		if(distanceCheck >= 200) then
+			degreeAdd = 5
+			rotation = 72
+		end
+		
+		if shoreFound ~= true then
+			emptyGroup(rayCastDistanceCheck)
+			emptyGroup(rayCastCheck)
+		end
+		
+		-- Reset degree
+		degree = 0
+		-- Increase distances checked
+		distanceCheck = distanceCheck + 25	
+		
+		---------------------------
+		------- DEBUG TEXT --------
+		--if foreverCheck > 20 then
+			--print("you just hit a forever loop, we getting out of here!!")
+		--	break
+		--end		
+		------- DEBUG TEXT --------
+		---------------------------
+	end
+	-- End while loop
+	
+	launch(rayCastCheck, ball, player, useLastPoint)
+end
 
-		local choosePoint = 0
-		local minDist = math.huge
-		for i = 1, rayCastCheck.numChildren do
-			local dist
-			if useLastPoint then
-				dist = uMath.distance(rayCastCheck[i], lastPoint)
-				--print("Hit: " .. i .. " " .. rayCastCheck[i].name .. " at position " .. rayCastCheck[i].x .. ", " .. rayCastCheck[i].y .. " distance: " .. dist)
+--------------------------------------------------------------------------------
+-- On Accelerate - function that gathers accelerometer data
+--------------------------------------------------------------------------------
+-- Updated by: Derrick
+-- Previous: Andrew 
+--------------------------------------------------------------------------------
+local function onAccelerate(event, player)
+	-- Print escape path
+	--print(player.escape)
+		
+	-- Accelerometer Tilt Events	
+	local xGrav, yGrav = calcAccelValues(event)
 				
-			else
-				dist = uMath.distance(rayCastCheck[i], ball)
-				--print("Hit: " .. i .. " " .. rayCastCheck[i].name .. " at position " .. rayCastCheck[i].x .. ", " .. rayCastCheck[i].y .. " distance: " .. dist)
-			end
-			if dist < minDist then
-				minDist = dist
-				choosePoint = i
-			end
-		end	
-
-		--print("Go to point : " .. choosePoint .. " at " .. rayCastCheck[choosePoint].x .. ", " .. rayCastCheck[choosePoint].y .. "with distance of " .. minDist)
-		rayCastCheck[choosePoint]:setFillColor(0,0,0)
-		rayCastCheck[choosePoint]:toFront()
-
-		local pushDistance = uMath.distance(ball, rayCastCheck[choosePoint])
-		local straightDistance = uMath.distance(ball, lastPoint)
-
-		local chooseDistance = pushDistance
-		local chooseLocation = rayCastCheck[choosePoint]
-
-		if pushDistance > straightDistance and useLastPoint then
-			chooseLocation = lastPoint
-			chooseDistance = straightDistance
-		end
-
-		local deltaX, deltaY = 0,0
-		deltaX = chooseLocation.x - ball.x
-		deltaY = chooseLocation.y - ball.y
-
-		local angleX = math.acos(deltaX/chooseDistance)
-		local angleY = math.asin(deltaY/chooseDistance)
-
-		local jumpDirectionX, jumpDirectionY = 0,0
-		jumpDirectionX = 20 * math.cos(angleX)
-		jumpDirectionY = 20 * math.sin(angleY)
-
-		--print("push player in " .. jumpDirectionX .. ", " .. jumpDirectionY)
-		accelPlayer[1] = player
-		player.shook = true
-		--ball:applyForce(jumpDirectionX, jumpDirectionY, ball.x, ball.y)
-		ball.linearDamping = 0
-		--local function pushDatNigga()
-		ball:setLinearVelocity(deltaX*3, deltaY*3)
-		ball.alpha = 1
-		--end
-		--timer.performWithDelay(2000, pushDatNigga)
-		--transition.to(ball, {time = 200, x = lastPoint.x, y = lastPoint.y})
-	elseif gameData.inWater == false or gameData.onIceberg == true then
+	if gameData.inWater == false or gameData.onIceberg == true then
 		player.imageObject.alpha = 1
 		--sound.playNarration(sound.soundEffects[7])
 		-- offset the gravity to return
@@ -298,6 +254,12 @@ local function onAccelerate(event, player)
 		--sound.pauseSound(1)
 		physicsParam.xGrav = 0
 		physicsParam.yGrav = 0
+		
+		if event.isShake and player.shook == false then
+			emptyGroup(rayCastCheck)
+			emptyGroup(rayCastDistanceCheck)
+			shake(player)
+		end
 	end
 	
 	--return physics parameters
