00054050fce2ed3d936a59c01a64b81e990a0db1
Derrick Wong <decwong@ucsc.edu>
2014-06-02 15:49:27 -0700

    Removing curse implementation in tutorial causing errors

--- Cocooned-master/CocoonedStable/utils/tutorialLib.lua
+++ Cocooned-master/CocoonedStable/utils/tutorialLib.lua
@@ -123,12 +123,12 @@ local function toggleNext(event)
 		
 		-- Resume physics
 		--physics.start();
-		if tempPlayer.small == true then
-			tempPlayer.curse = 0.5
-		else
-			tempPlayer.curse = 1
-		end
-		print("tempPlayer.curse", tempPlayer.curse)
+		--if tempPlayer.small == true then
+		--	tempPlayer.curse = 0.5
+		--else
+		--	tempPlayer.curse = 1
+		--end
+		--print("tempPlayer.curse", tempPlayer.curse)
 		
 		-- Resume game timer
 		gameTimer.resumeTimer()
@@ -152,10 +152,10 @@ function tutorialLib:showTipBox(tipType, value, gui, player)
 	-- Pause physics	
 	--physics.pause()
 	-- temporarily store player
-	tempPlayer = player
-	if tempPlayer.curse ~= 0 then
-		tempPlayer.curse = 0
-	end
+	--tempPlayer = player
+	--if tempPlayer.curse ~= 0 then
+	--	tempPlayer.curse = 0
+	--end
 
 	-- Pause game timer while tutorial screen is up
 	gameTimer.pauseTimer()
