d8c1b6739cd6a303c6887414807df94814deb5db
Derrick Wong <decwong@ucsc.edu>
2014-06-11 13:21:41 -0700

    Turning level complete back on

--- Cocooned-master/CocoonedStable/Objects/collision/exitPortalCollision.lua
+++ Cocooned-master/CocoonedStable/Objects/collision/exitPortalCollision.lua
@@ -42,8 +42,8 @@ local function collide(collideObject, player, event, mapData, map, gui)
 		player.imageObject:setLinearVelocity(0,0)
 		snow.meltSnow()
 		print("exiting")
-		--local complete = function()	cutScenes.endCutScene(gui, mapData); end
-		--local transPortal = transition.to(player.imageObject, {time=1000, alpha=0, x=collideObject.x, y=collideObject.y-15, onComplete = complete} )
+		local complete = function()	cutScenes.endCutScene(gui, mapData); end
+		local transPortal = transition.to(player.imageObject, {time=1000, alpha=0, x=collideObject.x, y=collideObject.y-15, onComplete = complete} )
 		gameData.gRune = false 
 	end
 	
