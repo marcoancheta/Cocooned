6991ceab7f54074252a41ade2e2d91ea5a1288fe
Derrick Wong <decwong@ucsc.edu>
2014-06-11 15:46:16 -0700

    Fixed mapping size for level 15

--- Cocooned-master/CocoonedStable/levels/world.lua
+++ Cocooned-master/CocoonedStable/levels/world.lua
@@ -96,7 +96,7 @@ local function load(mapData, map, rune, objects, wisp, water, wall, auraWall, pl
 		objects["exitPortal4"].x, objects["exitPortal4"].y = generate.tilesToPixels(20.5, 19)	
 		
 		-- Corona Simulator Accel Coordinates:
-		--objects["exitPortal3"].x, objects["exitPortal3"].y = generate.tilesToPixels(14, 12)	
+		objects["exitPortal3"].x, objects["exitPortal3"].y = generate.tilesToPixels(14, 12)	
 
 
 		-- set shadow angle for the pane
